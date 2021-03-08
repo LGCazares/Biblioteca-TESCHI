084cfc7730758fa04b1c5d5cb4b3c4d800949fb5
luisgerardocazares <luisgerardocazares@gmail.com>
2021-03-08 01:15:01 -0600

    Add: blade form registro alumno

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AdministradorController extends Controller
+
 {
-    //
+    public function storeAlumno(){
+        return view('registro.form_registro');
+    }
 }
